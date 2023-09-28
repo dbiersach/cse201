fca30280d1a7fe7c2753f2f836cdf44ffeb42248
David Biersach <dbiersach@gmail.com>
2023-09-28 16:28:16 -0400

    Update Session 01

--- labs/identify_element/identify_element.py
+++ labs/identify_element/identify_element.py
@@ -1,54 +1,40 @@
 # identify_element.py
 
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 
-def fit_linear(vec_x, vec_y):
-    vec_x = vec_x.reshape(-1, 1)
-    model = LinearRegression().fit(vec_x, vec_y)
-    m = model.coef_
-    return m
+def fit_linear(x, y):
+    m = len(x) * np.sum(x * y) - np.sum(x) * np.sum(y)
+    m = m / (len(x) * np.sum(x**2) - np.sum(x) ** 2)
+    b = (np.sum(y) - m * np.sum(x)) / len(x)
+    return m, b
 
 
-def main():
-    plt.figure(__file__)
-    ax = plt.axes()
+# Read experiment data from data file
+data = np.genfromtxt("gas.csv", delimiter=",")
 
-    vec_temperature_celsius = np.array([-50.0, 0.0, 50.0, 100.0, 150.0])
-    vec_temperature_kelvin = vec_temperature_celsius + 273.15
+# Convert experiment data to SI units
+temperature = data[:, 0] + 273.15  # 1st column to kelvin
+volume = data[:, 1] / 1000  # 2nd column to meters cubed
 
-    vec_volume_liters = np.array([11.6, 14.0, 16.2, 19.4, 21.8])
-    vec_volume_meters_cubed = vec_volume_liters / 1000
+# Calculate line of best fit
+slope, yint = fit_linear(temperature, volume)
 
-    m = fit_linear(vec_temperature_kelvin, vec_volume_meters_cubed)
+# Apply Ideal gas law
+p = 2.0 * 101_325  # Convert 2.0 atm (given) to pascals
+r = 8.31446261815324  # Gas constant (SI units)
+n = p / r * slope  # Moles of gas (rearrange ideal gas law equation)
 
-    slope = m[0]  # Ratio of V to T (slope of best-fit V/T line)
+# Calculate atomic mass of sample using number of moles
+m_sample = 50  # (given) grams
+molar_mass = m_sample / n  # sample mass divided by number of moles
 
-    p = 2.0 * 101_325  # Convert atm to pascals
-    r = 8.31446261815324  # Ideal gas constant (m^3*Pa/(K*mol))
-    n = p / r * slope  # Moles of gas (rearrange ideal gas law equation)
-
-    m_sample = 50  # grams
-    molar_mass = m_sample / n  # sample mass divided by number of moles
-
-    print(f"Molar mass of Unknown Gas: {molar_mass:.3f}u")
-    print("      Molar mass of Argon: 39.948u")
-
-    x = np.linspace(0, 500, 1000)
-    ax.plot(x, m * x)
-    ax.scatter(vec_temperature_kelvin, vec_volume_meters_cubed, color="red")
-    ax.set_title(f"Unknown Gas ({molar_mass:.3f}u)")
-    ax.set_xlabel(r"$Temperature\;(\degree K)$")
-    ax.set_ylabel(r"$Volume\;(m^3)$")
-    ax.set_xlim(0, 500)
-    ax.set_ylim(0, 0.025)
-
-    ax.figure.set_size_inches(10, 8)
-    plt.savefig("identify_element.png", dpi=600)
-
-    plt.show()
-
-
-main()
+plt.figure()
+plt.scatter(temperature, volume, color="red")
+t = np.linspace(0, 500)
+plt.plot(t, slope * t + yint)
+plt.title(f"Unknown Gas ({molar_mass:.3f}u)")
+plt.xlabel("Temperature (K)")
+plt.ylabel("Volume (m^3)")
+plt.show()
--- labs/plot_equation/plot_equation.py
+++ labs/plot_equation/plot_equation.py
@@ -3,34 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+V_s = 3.3  # in Volts
+R = 10_000  # 10K Ohms in Ohms
+C = 1e-5  # 10 uF in Farads
+tau = R * C
 
-def main():
-    plt.figure(__file__)
-    ax = plt.axes()
-    ax.set_facecolor("black")
+t = np.linspace(0, 1, 100)
+v_c = V_s * (1 - np.exp(-t / tau))
 
-    V_s = 3.3  # in Volts
-    R = 10_000  # 10K Ohms in Ohms
-    C = 1e-5  # 10 uF in Farads
-    tau = R * C
-
-    t = np.linspace(0, 1, 100)
-    v_c = V_s * (1 - np.exp(-t / tau))
-
-    ax.plot(t, v_c, color="cyan", linewidth=2)
-
-    ax.set_title("Capacitor Voltage vs. Time")
-    ax.set_xlabel("Time (s)")
-    ax.set_ylabel("Voltage (V)")
-
-    ax.axhline(V_s, color="yellow", linewidth=2)
-
-    ax.grid(which="both", color="gray", linestyle="dotted", alpha=0.5)
-    
-    ax.figure.set_size_inches(10, 8)
-    plt.savefig("plot_equation.png", dpi=600)    
-
-    plt.show()
-
-
-main()
+plt.figure()
+plt.gca().set_facecolor("black")
+plt.plot(t, v_c, color="cyan", linewidth=2)
+plt.title("Capacitor Voltage vs. Time")
+plt.xlabel("Time (s)")
+plt.ylabel("Voltage (V)")
+plt.axhline(V_s, color="yellow", linewidth=2)
+plt.grid(which="both", color="gray", linestyle="dotted", alpha=0.5)
+plt.show()
